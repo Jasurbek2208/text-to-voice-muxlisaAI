209c2ff49f6d75884bd1546b02dfdbab8f1e3db0
Jasur <jasurbekshomaqsudov2004@gmail.com>
2023-09-26 15:40:35 +0300

    feat: Loader tsx component, fix: routing as adding vercel.json file and codes

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,13 +2,17 @@ import React, { Suspense } from "react";
 import { useRoutes } from "react-router-dom";
 import { useTypedSelector } from "../hooks/reduxSelector";
 
+// Routes
 import { authRoutes, routes } from "./routesLinks";
+
+// Loader
+import Loader from "../components/loader/Loader";
 
 export default function Router() {
   const { user: { isAuth } } = useTypedSelector((store) => store?.store);
 
   const currentRoutes = isAuth ? routes : authRoutes;
   const routers = useRoutes(currentRoutes);
-  
-  return <Suspense fallback={<h1 className="text-center">Loading...</h1>}>{routers}</Suspense>;
+
+  return <Suspense fallback={<Loader />}>{routers}</Suspense>;
 }
