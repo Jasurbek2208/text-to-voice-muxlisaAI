27fa7aca5b4d9dddc0661d258d1c5d7220e2d890
Jasur <jasurbekshomaqsudov2004@gmail.com>
2023-10-02 18:24:52 +0300

    feat: basic custom audio styles

--- src/components/message/VoiceMessage.tsx
+++ src/components/message/VoiceMessage.tsx
@@ -1,5 +1,5 @@
 import { getCurrentAudio } from "@helpers/muxlisaAIAPI";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 // Types
 import { IMessageConfigs } from "../../types";
@@ -12,14 +12,37 @@ interface IMessage {
 
 export default function VoiceMessage({ message, isUser, rounded }: IMessage) {
   const currentAudioRef = useRef<HTMLAudioElement>(null);
+  const [audioStream, setAudioStream] = useState(new Audio('YOUR_AUDIO_STREAM_URL'));
+  
+  useEffect(() => {
+    console.log(currentAudioRef);
+
+  },[currentAudioRef])
+  
 
   return (
     <>
-      <div onClick={() => getCurrentAudio(currentAudioRef, message?.value || "")}>Get this audio</div>
-      <audio ref={currentAudioRef} controls className={`${isUser ? 'self-end sm:mr-9 mr-3' : 'self-start sm:ml-9 ml-3'} block sm:max-w-xs max-w-[60%] w-full p-2.5 bg-gray-100 border border-gray-200 rounded-t-xl ${rounded} shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700`}>
-        <source src={message?.value || "audio"} type="audio/*" />
-          Brauzeringiz audio elementini qo'llab-quvvatlamaydi.
-      </audio>
+      <div className={`${isUser ? 'self-end sm:mr-9 mr-3' : 'self-start sm:ml-9 ml-3'} block sm:max-w-xs max-w-[60%] w-full p-3 bg-gray-100 border border-gray-200 rounded-t-xl ${rounded} shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700`}>
+      {/* <div onClick={() => getCurrentAudio(currentAudioRef, message?.value || "")}>Get this audio</div> */}
+
+        <div className="flex items-center justify-between">
+          <audio ref={currentAudioRef}>
+            <source src={message?.value || "audio"} type="audio/*" />
+              Brauzeringiz audio elementini qo'llab-quvvatlamaydi.
+          </audio>
+          <div className="w-5 h-5 bg-dark cursor-pointer" onClick={() => !currentAudioRef?.current?.src ? getCurrentAudio(currentAudioRef, message?.value || "") : null}></div>
+
+          <span className="text-neutral-900">{currentAudioRef?.current?.currentTime || "0:00"} / {currentAudioRef?.current?.duration || "0:00"}</span>
+
+
+          <div className="p-1.5 transition hover:bg-zinc-200 rounded-full">
+            <div className="three-dost w-5 h-5 cursor-pointer"></div>
+          </div>
+        </div>
+
+        <progress className="m-0 w-full rounded-full cursor-pointer" value="5" max="8"></progress>
+
+      </div>
     </>
   )
 }
